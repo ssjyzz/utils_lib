5c416575f694afa70ca9942290e5acffb9b40cc2
betty.gao <ssjy66@126.com>
2022-07-13 16:08:33 +0800

    feat: 添加封装好的公共方法

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,29 @@
+import isColor from "./isColor";
+import debounce from "./debounce";
+import throttle from "./throttle";
+import getCookie from "./getCookie";
+import deleteCookie from "./deleteCookie";
+import setCookie from "./setCookie";
+import filterEmptyValue from "./filterEmptyValue";
+import getDateStr from "./getDateStr";
+import removeAllSpace from "./removeAllSpace";
+import isInteger from "./isInteger";
+import arrayUnique from "./arrayUnique";
+import arrIsEqualContent from "./arrIsEqualContent";
 import deepClone from "./deepClone";
 
 export {
-  deepClone
+  isColor,
+  debounce,
+  throttle,
+  getCookie,
+  deleteCookie,
+  setCookie,
+  filterEmptyValue,
+  getDateStr,
+  removeAllSpace,
+  isInteger,
+  arrayUnique,
+  arrIsEqualContent,
+  deepClone,
 };
